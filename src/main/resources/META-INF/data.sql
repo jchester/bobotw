66d7ba8a2a3df66638fde5a70df9050f63854c4c
Jacques Chester <github@jacques.chester.id.au>
2024-10-23 14:42:34 -0700

    Add some real data

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -1,13 +1,19 @@
 -- @formatter:off
-insert into episodes(id, title) values (1, 'Best of the Worst');
-insert into episodes(id, title) values (2, 'Best of the Worst 2');
-insert into episodes(id, title) values (3, 'Best of the Worst 3');
-insert into episodes(id, title) values (4, 'Best of the Worst 4');
+insert into episodes(id, title) values (1, 'Episode 1');
+insert into episodes(id, title) values (2, 'Lady Terminator, Lost in Dinosaur World, and Low Blow');
+insert into episodes(id, title) values (3, 'The Killer Eye, They Bite, and Xtro');
+insert into episodes(id, title) values (4, 'Deadly Prey, Hard Ticket to Hawaii, and Miami Connection');
+insert into episodes(id, title) values (5, 'The Wheel of the Worst');
+insert into episodes(id, title) values (6, 'The Vindicator, Cyber Tracker, Robot Jox, and R.O.T.O.R.');
+insert into episodes(id, title) values (7, 'Playing Dangerous, Shapeshifter, and Thunderpants');
+insert into episodes(id, title) values (8, 'Wheel of the Worst #2');
+insert into episodes(id, title) values (9, 'V-World Matrix, The Amazing Bulk, and ????');
+insert into episodes(id, title) values (10, 'Bloody Birthday, Crazy Fat Ethel II, and Psycho From Texas');
 
-insert into videos(id, title, episode_id) values (1, 'A likely title', 1);
-insert into videos(id, title, episode_id) values (2, 'Another one', 2);
-insert into videos(id, title, episode_id) values (3, 'The third', 3);
-insert into videos(id, title, episode_id) values (4, 'Keepsake', 4);
+
+insert into videos(id, title, episode_id) values (1, 'Russian Terminator', 1);
+insert into videos(id, title, episode_id) values (2, 'Xtro', 2);
+insert into videos(id, title, episode_id) values (3, 'Miami Connection', 3);
 
 insert into rankers(id) values ('C5C8C703-5730-4511-BA55-224DD955E20D');
 
