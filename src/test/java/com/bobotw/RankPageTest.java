62a0391b50b114f20f3729029c0a56e3b7dd81e0
Jacques Chester <github@jacques.chester.id.au>
2024-10-23 16:24:27 -0700

    Create new Ranker if cookie unset

--- src/test/java/com/bobotw/RankPageTest.java
+++ src/test/java/com/bobotw/RankPageTest.java
@@ -2,13 +2,17 @@ package com.bobotw;
 
 import org.htmlunit.WebClient;
 import org.htmlunit.html.HtmlPage;
+import org.htmlunit.util.Cookie;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 public class RankPageTest {
     private final WebClient webClient = new WebClient();
@@ -26,5 +30,21 @@ public class RankPageTest {
     void hasTwoOptions() {
         assertThat(page.getElementById("option-1"), notNullValue());
         assertThat(page.getElementById("option-2"), notNullValue());
+    }
+
+    @Test
+    void itSetsACookie() throws URISyntaxException, MalformedURLException {
+        Set<Cookie> cookies = webClient.getCookies(new URI("http://localhost:8080/rank").toURL());
+
+        assertThat(cookies, hasSize(1));
+
+        Cookie cookie = cookies.stream().findFirst().get();
+
+        assertThat(cookie.getName(), is("bobotw"));
+        assertThat(cookie.getDomain(), is("localhost")); // TODO make this general for bobotw.com
+        assertThat(cookie.getPath(), is("/rank"));
+        assertThat(cookie.isHttpOnly(), is(true));
+        assertThat(cookie.isSecure(), is(true));
+        assertThat(cookie.getSameSite(), is("strict"));
     }
 }
