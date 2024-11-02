59cdabfb5b4a1a2df29ba59b6241708a94035025
Jacques Chester <github@jacques.chester.id.au>
2024-11-02 10:58:04 -0700

    Add videos to data, remove initial rankings

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,10 @@
 -- @formatter:off
 insert into videos(id, title, episode_title) values (1, 'Russian Terminator', 'Episode 1');
-insert into videos(id, title, episode_title) values (2, 'The New Gladiators', 'The New Gladiators, Exterminator 2, and The Aftermath');
+insert into videos(id, title, episode_title) values (2, 'Aftermath', 'The New Gladiators, Exterminator 2, and The Aftermath');
 insert into videos(id, title, episode_title) values (3, 'Xtro', 'The Killer Eye, They Bite, and Xtro');
 insert into videos(id, title, episode_title) values (4, 'Miami Connection', 'Deadly Prey, Hard Ticket to Hawaii, and Miami Connection');
-
-insert into rankers(id) values ('C5C8C703-5730-4511-BA55-224DD955E20D');
-
-insert into pair_rankings(ranker_id, winner_id, loser_id) values ('C5C8C703-5730-4511-BA55-224DD955E20D', 1, 3);
-insert into pair_rankings(ranker_id, winner_id, loser_id) values ('C5C8C703-5730-4511-BA55-224DD955E20D', 1, 4);
-insert into pair_rankings(ranker_id, winner_id, loser_id) values ('C5C8C703-5730-4511-BA55-224DD955E20D', 3, 2);
+insert into videos(id, title, episode_title) values (5, 'Let''s Rap About Fire Safety', 'The Wheel of the Worst');
+insert into videos(id, title, episode_title) values (6, 'R.O.T.O.R.', 'The Vindicator, Cyber Tracker, Robot Jox, and R.O.T.O.R.');
+insert into videos(id, title, episode_title) values (7, 'Thunderpants', 'Playing Dangerous, Shapeshifter, and Thunderpants');
+insert into videos(id, title, episode_title) values (8, 'Gary Coleman For Safety''s Sake', 'Wheel of the Worst #2');
 
