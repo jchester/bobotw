2bc7359abab2ce9b93a0d84dc7450c508187ff58
Jacques Chester <github@jacques.chester.id.au>
2024-11-02 11:34:37 -0700

    Add more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,11 +6,16 @@ values (1, 'Russian Terminator', 'Episode 1'),
        (5, 'Let''s Rap About Fire Safety', 'The Wheel of the Worst'),
        (6, 'R.O.T.O.R.', 'The Vindicator, Cyber Tracker, Robot Jox, and R.O.T.O.R.'),
        (7, 'Thunderpants', 'Playing Dangerous, Shapeshifter, and Thunderpants'),
-       (8, 'Gary Coleman For Safety''s Sake', 'Wheel of the Worst #2');
+       (8, 'Gary Coleman For Safety''s Sake', 'Wheel of the Worst #2'),
+       (9, 'Gymkata', 'V-World Matrix, The Amazing Bulk, and ????')
+;
 
 insert into tags(id, text)
-values (1, 'Wheel Episode');
+values (1, 'Wheel of the Worst'),
+       (2, 'Colin from Canada');
 
 insert into tags_on_videos(video_id, tag_id)
 values (5, 1),
-       (8, 1);
+       (8, 1),
+       (9, 2)
+;
