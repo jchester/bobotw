a323198029f9fa310b145e09b01c42b87fd781a3
Jacques Chester <github@jacques.chester.id.au>
2024-11-02 11:04:27 -0700

    Reformat insert statements

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
--- @formatter:off
-insert into videos(id, title, episode_title) values (1, 'Russian Terminator', 'Episode 1');
-insert into videos(id, title, episode_title) values (2, 'Aftermath', 'The New Gladiators, Exterminator 2, and The Aftermath');
-insert into videos(id, title, episode_title) values (3, 'Xtro', 'The Killer Eye, They Bite, and Xtro');
-insert into videos(id, title, episode_title) values (4, 'Miami Connection', 'Deadly Prey, Hard Ticket to Hawaii, and Miami Connection');
-insert into videos(id, title, episode_title) values (5, 'Let''s Rap About Fire Safety', 'The Wheel of the Worst');
-insert into videos(id, title, episode_title) values (6, 'R.O.T.O.R.', 'The Vindicator, Cyber Tracker, Robot Jox, and R.O.T.O.R.');
-insert into videos(id, title, episode_title) values (7, 'Thunderpants', 'Playing Dangerous, Shapeshifter, and Thunderpants');
-insert into videos(id, title, episode_title) values (8, 'Gary Coleman For Safety''s Sake', 'Wheel of the Worst #2');
+insert into videos(id, title, episode_title)
+values (1, 'Russian Terminator', 'Episode 1'),
+       (2, 'Aftermath', 'The New Gladiators, Exterminator 2, and The Aftermath'),
+       (3, 'Xtro', 'The Killer Eye, They Bite, and Xtro'),
+       (4, 'Miami Connection', 'Deadly Prey, Hard Ticket to Hawaii, and Miami Connection'),
+       (5, 'Let''s Rap About Fire Safety', 'The Wheel of the Worst'),
+       (6, 'R.O.T.O.R.', 'The Vindicator, Cyber Tracker, Robot Jox, and R.O.T.O.R.'),
+       (7, 'Thunderpants', 'Playing Dangerous, Shapeshifter, and Thunderpants'),
+       (8, 'Gary Coleman For Safety''s Sake', 'Wheel of the Worst #2');
 
